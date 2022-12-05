eaea14bdca26b452e3d03f11dc14af7823c1a523
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-05 19:42:18 +1000

    Delete Basic Website directory

--- Basic Website/footer_login.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo "<a href=\"home.php\" >Home</a> | ";
-	echo "<a href=\"login.php\">Login</a>";
-?>
--- Basic Website/footer_logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo "<a href=\"home.php\" >Home</a> | ";
-	echo "<a href=\"add.php\">Add New Animal</a> | ";
-	echo "<a href=\"logout.php\">Logout</a>";
-?>
